feb685c0358e53beea4656cb32f7cef2abf5c86b
tailorem <trm@inspire.ca>
2019-07-29 20:56:24 -0700

    Adds ping route

--- client/server.js
+++ client/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ip = '0.0.0.0';
 
-app.get('/', (req, res) => res.status(200).json({ hey: 'plan\'t'}));
+app.get('/', (req, res) => res.status(200).send('Hello world!'));
+app.get('/ping', (req, res) => res.status(200).json({ says: 'pong'}));
 
-app.listen(port, () => console.log(`Server listening on port ${port}...!`));
+app.listen(port, ip, () => console.log(`Server listening on port ${port}...!`));
