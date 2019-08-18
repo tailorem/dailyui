2c520e333b516307e2e898365e93a2c5ef4cfc33
Taylour <taylour@einstein.exchange>
2019-08-18 12:21:33 -0700

    Updates server start message

--- client/server.js
+++ client/server.js
@@ -3,14 +3,13 @@ const https = require('https');
 const express = require('express');
 
 const httpsOptions = {
-    cert: fs.readFileSync('../secrets/ssl/taylour_dev.crt'),
-    ca: fs.readFileSync('../secrets/ssl/taylour_dev.ca-bundle'),
-    key: fs.readFileSync('../secrets/ssl/taylour_dev.key')
+  cert: fs.readFileSync('../secrets/ssl/taylour_dev.crt'),
+  ca: fs.readFileSync('../secrets/ssl/taylour_dev.ca-bundle'),
+  key: fs.readFileSync('../secrets/ssl/taylour_dev.key')
 };
 
 const hostname = 'taylour.dev';
 const httpsPort = 443;
-// const ip = '0.0.0.0';
 
 const app = express();
 const httpsServer = https.createServer(httpsOptions, app);
@@ -18,5 +17,4 @@ const httpsServer = https.createServer(httpsOptions, app);
 app.get('/', (req, res) => res.status(200).send('Hello world!'));
 app.get('/ping', (req, res) => res.status(200).json({ says: 'pong'}));
 
-// app.listen(httpsPort, ip, () => console.log(`Server listening on httpsPort ${httpsPort}...!`));
-httpsServer.listen(httpsPort, hostname, () => console.log(`Server listening on httpsPort ${httpsPort}...!`));
+httpsServer.listen(httpsPort, hostname, () => console.log(`Server listening on port ${httpsPort}...!`));
