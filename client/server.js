da1136c134f31b361dc537b7c1f3aeeec1bd24e2
Taylour <taylour@einstein.exchange>
2019-08-18 09:33:49 -0700

    Adds https

--- client/server.js
+++ client/server.js
@@ -1,9 +1,22 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
+
+const httpsOptions = {
+    cert: fs.readFileSync('../secrets/ssl/taylour_dev.crt'),
+    ca: fs.readFileSync('../secrets/ssl/taylour_dev.ca-bundle'),
+    key: fs.readFileSync('../secrets/ssl/taylour_dev.key')
+};
+
+const hostname = 'taylour.dev';
+const httpsPort = 443;
+// const ip = '0.0.0.0';
+
 const app = express();
-const port = 3000;
-const ip = '0.0.0.0';
+const httpsServer = https.createServer(httpsOptions, app);
 
 app.get('/', (req, res) => res.status(200).send('Hello world!'));
 app.get('/ping', (req, res) => res.status(200).json({ says: 'pong'}));
 
-app.listen(port, ip, () => console.log(`Server listening on port ${port}...!`));
+// app.listen(httpsPort, ip, () => console.log(`Server listening on httpsPort ${httpsPort}...!`));
+httpsServer.listen(httpsPort, hostname);
